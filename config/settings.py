25e3cf6189eb7e836da412070d622fcdad372239
Vijayadhi <venerablevignesh@gmail.com>
2024-12-21 18:37:40 +0530

    commit-3

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,6 @@ from django.conf.global_settings import STATICFILES_DIRS
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -29,14 +28,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
-    'shgs.onrender.com',
+    "socialandmusicalchambertrust.onrender.com",
     '127.0.0.1',
     '35.160.120.126',
     '44.233.151.27',
     '34.211.200.85',
     # '0.0.0.0/0',
     '0.0.0.0/0']
-
 
 # Application definition
 
@@ -98,7 +96,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -108,7 +105,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -128,7 +124,6 @@ AUTH_PASSWORD_VALIDATORS = [
     # },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -139,7 +134,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
@@ -157,7 +151,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'backend.CustomUser'
 
 JAZZMIN_SETTINGS = {
-    "custom_js":"./admin.js"
+    "custom_js": "./admin.js"
 }
 
 CSRF_TRUSTED_ORIGINS = [
