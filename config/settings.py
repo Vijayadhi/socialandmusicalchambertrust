90c1be6adddc4c3d81bb0826851a1acfce666793
Vijayadhi <venerablevignesh@gmail.com>
2024-12-21 20:22:44 +0530

    commit11

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ CORS_ALLOW_HEADERS = [
 ]
 MIDDLEWARE = [
     # 'django.middleware.security.SecurityMiddleware',
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
